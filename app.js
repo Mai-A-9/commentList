d1041b87398985cfdb3364d24f353a8354b7ef5e
Mai-A-9 <hjil2847@yahoo.co.jp>
2021-04-11 21:35:11 +0900

    Add home and edit routing.
    ホーム画面と編集のルーティングを追加

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const methodOverride = require("method-override");
 mongoose.connect('mongodb://localhost/commentApp', {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
 });
 
 const db = mongoose.connection;
@@ -25,7 +26,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(methodOverride("_method"));
 
-app.get("/comments", async (req, res) => {
+app.get("/comments", (req, res) => {
+    res.render("home")
+});
+
+app.get("/comments/index", async (req, res) => {
     const comments = await Comment.find({});
     res.render("index", { comments });
 });
@@ -37,13 +42,26 @@ app.get("/comments/new", (req, res) => {
 app.post("/comments", async (req, res) => {
     const comment = new Comment(req.body);
     await comment.save();
-    res.redirect("/comments");
+    res.redirect("/comments/index");
 });
 
-app.delete("/comments/:id", async (req, res) => {
+app.delete("/comments/:id/delete", async (req, res) => {
     const { id } = req.params;
     await Comment.findByIdAndDelete(id);
-    res.redirect("/comments");
+    res.redirect("/comments/index");
+});
+
+app.get("/comments/:id/edit", async (req, res) => {
+    const { id } = req.params;
+    const comment = await Comment.findById(id);
+    res.render("edit", { comment });
+});
+
+app.put("/comments/:id/edit", async (req, res) => {
+    const text = req.body;
+    const { id } = req.params;
+    await Comment.findByIdAndUpdate(id, text);
+    res.redirect("/comments/index");
 });
 
 app.listen(3000, () => {
